35022466bb51abaaefc699b4450120d7afefc41e
sachika13125 <133827566+sachika13125@users.noreply.github.com>
2024-03-21 18:13:56 -0700

    added Home.jsx

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import './App.css';
 import React from 'react';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
+import Home from'./Components/Home';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <main>
+        <Home />
+      </main>
       <Footer />
     </div>
   );
---
+++ src/Components/Home.jsx
@@ -0,0 +1,83 @@
+import React from 'react';
+import styled from 'styled-components';
+
+export default function Home() {
+    const Home = styled.div`
+        top: 0;
+        left: 0;
+        width: 100vw;
+        background-color: #fffff;
+        z-index: -1;
+        background-image:  radial-gradient(#ef4565 2px, transparent 2px), radial-gradient(#90b4ce 2px, #ffffff 2px);
+        background-size: 40px 40px;
+        background-position: 0 0,20px 20px;
+
+        .wellcome {
+            max-width: 1024x;
+            margin: 0 auto;
+            padding: 110px 20px 110px;
+        }
+
+        .message {
+            max-width: 450px;
+            margin: 20vh auto; 
+        }
+
+        .message a {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            width: 100%;
+            padding: 2.0em 1.0em;
+            border: 1px solid #094067;
+            background: linear-gradient(to right, #ffffff 50%, #094067 50%);
+            background-size: 200% auto;
+            color: #094067;
+            font-size: 2rem;
+            text-decoration: none;
+            text-align: center;
+            transition: .3s;
+            position: relative;
+        }
+
+        .message a:after {
+            content: "";
+            position: absolute;
+            right: -5px;
+            bottom: -5px;
+            width: 100%;
+            height: 100%;
+            border-right: 1px solid #222222;
+            border-bottom: 1px solid #222222;
+        }
+
+        .message a:hover {
+            color: #ffffff;
+            background-position: -100% 0;
+        }
+
+        .woman{
+            position: absolute;
+            top: 50%;
+            left: 10%;
+            width: 20vw;
+        }
+
+        .media {
+            position: absolute;
+            top: 25%;
+            right: 10%;
+            width: 20vw;
+        }
+    `
+
+    return (
+        <Home>
+            <div className='wellcome'>
+                <div className='message'>
+                    <a href='/newsfeeds'>For Our Students</a>
+                </div>
+            </div>
+        </Home>
+    )
+};
