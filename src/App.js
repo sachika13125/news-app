10021533fcf3472c56607dda8e0c9fdf11b18f54
sachika13125 <133827566+sachika13125@users.noreply.github.com>
2024-03-25 21:59:41 -0700

    update App.js

--- src/App.js
+++ src/App.js
@@ -2,20 +2,14 @@ import './App.css';
 import React from 'react';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
-import Home from'./Components/Home';
-import NewsFeed from './Components/NeesFeed';
-import Favorites from './Components/Favorites';
-import Login from './Components/Login';
+import AppRouter from './AppRouter';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <main>
-        <Login />
-        <Home />
-        <NewsFeed />
-        <Favorites />
+        <AppRouter />
       </main>
       <Footer />
     </div>
