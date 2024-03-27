be405eea010d7908af1a1ff8b79b85472e3e6721
sachika13125 <133827566+sachika13125@users.noreply.github.com>
2024-03-27 12:51:37 -0700

    add SignIn.jsx

---
+++ src/Components/SignUp.jsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+
+function SignUp() {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSignUp = () => {
+    const userData = {
+        username,
+        password
+    };
+
+    //Send a sign in request to server
+    fetch('/api/signup', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(userData),
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Sign up failed');
+        }
+        console.log('Sign up successful');
+    })
+    .catch.error('Sign up error', error);
+  };
+
+  return (
+    <div>
+      <h2>Sign Up</h2>
+      <form onSubmit={handleSignUp}>
+        <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} placeholder="Username" />
+        <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Email" />
+        <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Password" />
+        <button type="submit">Sign Up</button>
+      </form>
+    </div>
+  );
+}
+
+export default SignUp;
