866e1f45aba9af668475eae23fd9518c77f738a1
sachika13125 <133827566+sachika13125@users.noreply.github.com>
2024-04-02 12:46:09 -0700

    add login system

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,46 +1,51 @@
 import React, { useState } from 'react';
+import { createClient } from '@supabase/supabase-js';
 import './Login.css';
 import loginGif from '../assets/bubble-gum-pink-phone-and-circle-with-check-mark-inside.gif';
 import boyGif from '../assets/bubble-gum-support.gif';
 
-function Login() {
-  const [username, setUsername] = useState('');
+const Login = () => {
+  const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [isLoggedIn, setIsLoggedIn] = useState(!!localStorage.getItem('isLoggedIn'));
+  const [error, setError] = useState('');
 
-  const handleLogin = () => {
-    if (username === 'userName' && password === 'password') {
-      localStorage.setItem('isLoggedIn', true);
-      setIsLoggedIn(true);
+  const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+  const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
+  const supabase = createClient(supabaseUrl, supabaseKey);
+
+  const handleLogin = async () => {
+    try {
+      const { user, error } = await supabase.auth.signIn({
+        email,
+        password,
+      });
+
+      if (error) {
+        throw error;
+      }
+
+      console.log('Login Succesful:', user);
+    } catch (error) {
+      console.error('Login Error:', error.message);
+      setError('Invalid Email or Password');
     }
-  };
-
-  const handleLogout = () => {
-    localStorage.removeItem('isLoggedIn');
-    setIsLoggedIn(false);
   };
 
   return (
     <div className='login-page'>
-        <h1>Log In</h1>
-      {isLoggedIn ? (
-        <div>
-            <p>The user is already logged in.</p>
-            <button onClick={handleLogout}>Log Out</button>
-        </div>
-      ) : (
-        <div className='login-form'>
-            <label>User Name:
-                <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
-            </label>
-            <br />
-            <label>Password:
-                <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-            </label>
-            <br />
-            <button onClick={handleLogin}>Log In</button>
-        </div>
-      )}
+      <h1>Log In</h1>
+      <div className='login-form'>
+        <label>Email:
+            <input type='email' value={email} onChange={(e) => setEmail(e.target.value)} placeholder='email' />
+        </label>
+        <br />
+        <label>Password:
+            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder='password' />
+        </label>
+        <br />
+        <button onClick={handleLogin}>Log In</button>
+        {error && <p className='error-message'>{error}</p>}
+      </div>
       <img className='phone' src={loginGif} alt='phone and circle with check mark inside'/>
       <img className='boy' src={boyGif} alt='a boy flying in the sky'/>
     </div>
